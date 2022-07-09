4b886620600763acc86f88349ca453fbc310649f
DaiTou567 <vh95vvhh@gmail.com>
2022-07-08 22:12:24 -0700

    created file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Hardware_Drive.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware_Drive {
+
+    //declaring motors
+    public DcMotorEx lFront = null;
+    public DcMotorEx rFront = null;
+    public DcMotorEx lBack = null;
+    public DcMotorEx rBack = null;
+
+    //declaring servos
+    public Servo intake = null;
+    public CRServo continiousServo = null;
+
+
+}
