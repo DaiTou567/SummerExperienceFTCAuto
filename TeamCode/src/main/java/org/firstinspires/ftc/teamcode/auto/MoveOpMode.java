80d5bbef53e5d49f71822f73789a9f47c3d595a1
DaiTou567 <vh95vvhh@gmail.com>
2022-07-14 16:17:55 -0700

    new opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MoveOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import org.firstinspires.ftc.teamcode.auto.ConstantHeading;
+import org.firstinspires.ftc.teamcode.common.Hardware_Drive;
+import org.firstinspires.ftc.teamcode.common.Constants;
+
+public class MoveOpMode {
+    //declaring local class objects
+    Hardware_Drive hDrive = new Hardware_Drive();
+    Constants constants = new Constants();
+    ConstantHeading constantHeading = new ConstantHeading();
+}
